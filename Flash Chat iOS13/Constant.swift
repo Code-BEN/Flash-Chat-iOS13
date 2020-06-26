912856cdca88815194642c50471a7af39d2143f3
Code-BEN <bunhun2113@gmail.com>
2020-06-26 18:30:46 +0900

    first commit

---
+++ Flash Chat iOS13/Constant.swift
@@ -0,0 +1,9 @@
+//
+//  Constant.swift
+//  Flash Chat iOS13
+//
+//  Created by Leebyeonghoon on 2020/06/14.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Leebyeonghoon on 2020/06/16.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
